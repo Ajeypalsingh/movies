ba99181cadf2d3c249468ac3307e6555dce6fb23
Ajaypal Singh <122804961+Ajeypalsingh@users.noreply.github.com>
2023-05-04 17:56:34 -0500

    push proj.

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,10 +1,21 @@
 "use strict";
 
-console.log("hello");
-print("hello");
+import { select, print, onEvent } from "./utils.js";
 
-import { select, print } from "./utils.js";
+// selecting Inputs
+const moviesInput = select(".search-movie");
+const cityInput = select(".search-city");
+let listmovies = select(".autoComplete-movie");
+let listCities = select(".autoComplete-city");
 
+moviesInput.value = "";
+cityInput.value = "";
+
+// URLs for external json files
+const moviesUrl = "./assets/script/movies.json";
+const citiesUrl = "./assets/script/cities.json";
+
+// Options for fetch
 const options = {
   method: "GET",
   headers: {
@@ -13,11 +24,9 @@ const options = {
   mode: "cors",
 };
 
-const url = "./assets/script/movies.json";
-
 async function getMovies() {
   try {
-    const response = await fetch(url, options);
+    const response = await fetch(moviesUrl, options);
 
     if (!response.ok)
       throw new Error(`${response.statusText} ${response.status}`);
@@ -26,25 +35,108 @@ async function getMovies() {
     const grid = select(".grid-container");
     console.log(grid);
 
-    function get(array) {
+    function getInfo(array) {
       grid.innerHTML = "";
 
       array.forEach((element) => {
         grid.innerHTML += ` <div class="grid-item">
-               <div class="poster">
-                 <img
-                   src="${element.img}"
-                   alt="movie-poster"
-                 />
-               </div>
-               <p class="movie-title">${element.title}</p>
-       `;
+        <div class="poster">
+        <img
+        src="${element.img}"
+        alt="movie-poster"
+        />
+        </div>
+        <p class="movie-title">${element.title}</p>
+        `;
       });
     }
-    get(data.results);
+    getInfo(data.results);
+
+    // Functions for auto-completion inputs
+    function autoInputs(array, input) {
+      onEvent(input, "keyup", function () {
+        let inputValue = input.value.toLowerCase();
+        removeDropdown(moviesInput);
+
+        if (inputValue.length === 0) return;
+
+        listmovies.innerHTML = " ";
+
+        array.forEach((el) => {
+          if (
+            el.title.substr(0, inputValue.length).toLowerCase() === inputValue
+          ) {
+            listmovies.innerHTML += `<li><button>${el.title}</button></li>`;
+          }
+        });
+      });
+    }
+    autoInputs(data.results, moviesInput);
   } catch (error) {
     print(error.message);
   }
 }
 
 getMovies();
+
+// Remove dropdown
+function removeDropdown(input) {
+  if (input.value.length === 0) listmovies.innerHTML = " ";
+}
+
+// getting input by clicking on list
+function onclickMovies(e) {
+  e.preventDefault();
+
+  const click = e.target;
+  this.value = click.innerHTML;
+  listmovies.innerHTML = "";
+}
+
+onEvent(listmovies, "click", onclickMovies.bind(moviesInput));
+
+function onclickCities(e) {
+  e.preventDefault();
+
+  const click = e.target;
+  this.value = click.innerHTML;
+  listCities.innerHTML = "";
+}
+
+onEvent(listCities, "click", onclickCities.bind(cityInput));
+
+// fetching cities
+
+async function getCities() {
+  try {
+    const response = await fetch(citiesUrl, options);
+
+    if (!response.ok)
+      throw new Error(`${response.statusText} ${response.status}`);
+
+    const data = await response.json();
+
+    function autoInput(array, input) {
+      onEvent(input, "keyup", function () {
+        let inputValue = input.value.toLowerCase();
+
+        if (inputValue.length === 0) return;
+
+        listCities.innerHTML = " ";
+
+        array.forEach((el) => {
+          if (
+            el.name.substr(0, inputValue.length).toLowerCase() === inputValue
+          ) {
+            listCities.innerHTML += `<li><button>${el.name}</button></li>`;
+          }
+        });
+      });
+    }
+    autoInput(data.cities, cityInput);
+  } catch (error) {
+    print(error.message);
+  }
+}
+
+getCities();
